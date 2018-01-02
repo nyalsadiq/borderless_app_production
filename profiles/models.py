96e20e86ccc8f72a4dd06e51e08531dbe9128793
Nyal Sadiq <nyal@Nyals-MBP.home>
2018-01-02 20:56:11 +0000

    -Added profiles to admin -Added __str__() method to Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -10,6 +10,8 @@ class Profile(models.Model):
     location = models.CharField(max_length=30, blank=True)
     # birth_date = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return self.user.username
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
