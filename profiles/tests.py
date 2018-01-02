489ef4f7b257dad3b74edb213e4c77c59ee151c6
ichbinjon <jpgmend@gmail.com>
2018-01-02 18:56:56 +0100

    HTTP request added

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Profile
+from django.test import Client
+from django.urls import reverse
 
 
 class ProfileModelTests(TestCase):
@@ -18,3 +20,24 @@ class ProfileModelTests(TestCase):
         user.save()
 
         self.assertEqual(Profile.objects.get(pk=user.id).location, "Edinburgh")
+
+
+class ProfileModel_HTTPTests(TestCase):
+
+    def test_no_profiles(self):
+        c = Client()
+        response = c.get(reverse('profiles:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['user_list'], [])
+
+    def test_one_profile(self):
+        User.objects.create_user(
+            username="koalabear",
+            email="koalabear@example.com",
+            password="secret")
+
+        c = Client()
+        response = c.get(reverse('profiles:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['user_list']), 1)
+        self.assertEqual(response.context['user_list'][0].username, "koalabear")
