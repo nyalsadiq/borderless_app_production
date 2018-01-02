1524b8dd3061fa1ce5776b0dc618e11109b3b255
ichbinjon <jpgmend@gmail.com>
2018-01-02 18:23:03 +0100

    test added

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileModelTests(TestCase):
+
+    def test_profile_created(self):
+        user = User.objects.create_user(
+            username="koalabear",
+            email="koalabear@example.com",
+            password="secret")
+
+        self.assertEqual(User.objects.get(username="koalabear").email,
+                         "koalabear@example.com")
+
+        user.profile.location = "Edinburgh"
+        user.save()
+
+        self.assertEqual(Profile.objects.get(pk=user.id).location, "Edinburgh")
