33942523dc4cea0fee09e11a56e4c29121725e40
Nyal Sadiq <nyal@Nyals-MBP.home>
2018-01-06 18:59:16 +0000

    -Refactoring

--- projects/tests.py
+++ projects/tests.py
@@ -10,29 +10,43 @@ def create_requirements(text, project):
     return Requirement.objects.create(text=text, project=project)
     
 class IndexPageTests(TestCase):
-
+    """
+        Tests for appropriate responses from index view.
+    """
     def test_to_string(self):
         project = Project(title="Star Wars", description="Need a cameraman.", location="Edinburgh")
         self.assertEqual(project.to_string(), "Star Wars Need a cameraman.")
 
     def test_no_projects(self):
+        """
+            Tests to see if we get an appropriate response to no projects.
+        """
         response = self.client.get(reverse('projects:index'))
+
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No projects are available.")
         self.assertQuerysetEqual(response.context['project_list'], [])
 
     def test_one_project(self):
+        """
+            Tests for appropriate response for one project.
+        """
         create_project(title="Star Wars", description="Need a cameraman.", location="Edinburgh")
         response = self.client.get(reverse('projects:index'))
+
         self.assertQuerysetEqual(
             response.context['project_list'],
             ['<Project: Star Wars>']
         )
 
     def test_two_projects(self):
+        """
+            Tests for appropriate response for two projects.
+        """
         create_project(title="Star Wars", description="Need a cameraman.", location="Edinburgh")
         create_project(title="Sea Photoshoot", description="Need a diver.", location="Edinburgh")
         response = self.client.get(reverse('projects:index'))
+
         self.assertQuerysetEqual(
             response.context['project_list'],
             ['<Project: Star Wars>','<Project: Sea Photoshoot>'],
@@ -40,17 +54,28 @@ class IndexPageTests(TestCase):
         )  
 
 class DetailPageTests(TestCase):
+    """
+        Tests for appropriate responses from detail view.
+    """
 
     def test_no_requirements(self):
+        """
+            Tests for good response when there are no requirements.
+        """
         project = create_project(title="Star Wars", description="Need a cameraman.", location="Edinburgh")
         response = self.client.get(reverse('projects:detail', args=(project.id,)))
+
         self.assertContains(response, "No requirements")
         self.assertEqual(response.context['project'].title, "Star Wars")          
 
     def test_one_requirement(self):
+        """
+            Tests for good response when there is one requirement.
+        """
         project = create_project(title="Star Wars", description="Need a cameraman.", location="Edinburgh")
         requirement = create_requirements(text="Camera", project=project)
         response = self.client.get(reverse('projects:detail', args=(project.id,)))
+        
         self.assertEqual(response.context['project'].title, "Star Wars")
         self.assertQuerysetEqual(
             response.context['requirements'],
--- projects/views.py
+++ projects/views.py
@@ -3,54 +3,76 @@ from .models import Project, Requirement
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
 from django.urls import reverse
-# Create your views here.
 
 
-#Return list of all projects
 class IndexView(generic.ListView):
     context_object_name = 'project_list'
     template_name='projects/index.html'
 
+    
     def get_queryset(self):
+        """
+            GET returns queryset of all projects
+        """
         return Project.objects.all()
 
     def post(self, request, *args, **kwargs):
+        """
+            POST creates a new project with title and description parameters
+        """
         project_title = request.POST['project_title']
         description = request.POST['description']
+
         Project.objects.create(title = project_title, description=description)
+
         return HttpResponseRedirect(reverse('projects:index'))
 
 class ProjectView(View):
     context_object_name = 'project'
     template_name='projects/details.html'
 
-    #Return details of one project
     def get(self, request, *args, **kwargs):
+        """
+            Return details of one project
+        """
         project_id = kwargs.get('project_id')
+
         project = get_object_or_404(Project, pk=project_id)
         requirements = Requirement.objects.filter(project=project.id)
+
         return render(request, 'projects/details.html',{'project':project, 'requirements':requirements})
 
-    #PATCH - Update title/description field
+
     def post(self, request, *args, **kwagrs):
+        """
+            PATCH updates title/description field of specific project
+            DELETE deletes specific project
+            The request to be used (patch/delete) is posted via the 'field' hidden input
+        """
         method = request.POST['method']
         project_id = request.POST['project_id']
+
         project = get_object_or_404(Project, pk=project_id)
 
         if (method == 'PATCH'):
             field = request.POST['field']
             new_value = request.POST['new_value']
+
             if (field == "title"):
                 project.title = new_value
                 project.save()
+
             elif (field == "description"):
                 project.description = new_value
                 project.save()
+
             else:
                 return HttpResponseForbidden("Invalid Request.")
+
         elif (method == 'DELETE'):
             project.delete()
             return HttpResponseRedirect(reverse('projects:index'))
+
         else:
             return HttpResponseForbidden("Invalid Request.")
 
