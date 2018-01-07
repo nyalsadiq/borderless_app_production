b0193cf3a4d607e9343e138b556b3b7f65a81eef
Nyal Sadiq <nyal@Nyals-MBP.home>
2018-01-07 02:16:51 +0000

    -Converted to JSON Responses -Neater handling of DELETE/PATCH requests -Request data access fixes.

--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,4 @@ app_name = 'projects'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:project_id>/', views.ProjectView.as_view(), name='detail'),
-    #path('create/', views.CreateView.as_view(), name='create'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,21 +1,24 @@
 from django.views import generic, View
 from .models import Project, Requirement
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.http import JsonResponse, HttpResponseForbidden, HttpResponse, QueryDict
+from django.core import serializers
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 
-class IndexView(generic.ListView):
+class IndexView(View):
     context_object_name = 'project_list'
-    template_name='projects/index.html'
 
-    
-    def get_queryset(self):
+    def get(self, request):
         """
             GET returns queryset of all projects
         """
-        return Project.objects.all()
+        project_list = Project.objects.all()
+        response = serializers.serialize("json", project_list)
+        return HttpResponse(response, content_type='application/json')
 
+    
     def post(self, request, *args, **kwargs):
         """
             POST creates a new project with title and description parameters
@@ -25,11 +28,10 @@ class IndexView(generic.ListView):
 
         Project.objects.create(title = project_title, description=description)
 
-        return HttpResponseRedirect(reverse('projects:index'))
+        return JsonResponse({'status': 'created'})
 
 class ProjectView(View):
     context_object_name = 'project'
-    template_name='projects/details.html'
 
     def get(self, request, *args, **kwargs):
         """
@@ -40,45 +42,45 @@ class ProjectView(View):
         project = get_object_or_404(Project, pk=project_id)
         requirements = Requirement.objects.filter(project=project.id)
 
-        return render(request, 'projects/details.html',{'project':project, 'requirements':requirements})
+        project_json = serializers.serialize("json", [project])
+        requirements_json = serializers.serialize("json", requirements)
 
+        response = [project_json,requirements_json]
+        return HttpResponse(response, content_type='application/json')
+    
 
-    def post(self, request, *args, **kwagrs):
+    def post(self, request, *args, **kwargs):
         """
             PATCH updates title/description field of specific project
             DELETE deletes specific project
             The request to be used (patch/delete) is posted via the 'field' hidden input
         """
-        method = request.POST['method']
-        project_id = request.POST['project_id']
+        project_id = kwargs.get('project_id')
+        
+        field = request.POST['field']
+        new_value = request.POST['new_value']
 
         project = get_object_or_404(Project, pk=project_id)
 
-        if (method == 'PATCH'):
-            field = request.POST['field']
-            new_value = request.POST['new_value']
-
-            if (field == "title"):
-                project.title = new_value
-                project.save()
-
-            elif (field == "description"):
-                project.description = new_value
-                project.save()
-
-            else:
-                return HttpResponseForbidden("Invalid Request.")
-
-        elif (method == 'DELETE'):
-            project.delete()
-            return HttpResponseRedirect(reverse('projects:index'))
-
+        if (field == "title"):
+            project.title = new_value
+            project.save()
+        elif (field == "description"):
+            project.description = new_value
+            project.save()
         else:
-            return HttpResponseForbidden("Invalid Request.")
+            return JsonResponse({'status':'invalid'})
 
-        return HttpResponseRedirect(reverse('projects:detail', args=(project_id,)))
+        return JsonResponse({'status':'updated'})
+
+    def delete(self, request, *args, **kwargs):
+        project_id = kwargs.get('project_id')
+        project = get_object_or_404(Project, pk=project_id)
+        project.delete()
+        return JsonResponse({'status':'deleted'})
 
 
     
+
 
     
