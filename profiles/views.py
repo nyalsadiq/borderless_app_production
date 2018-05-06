dc3de266e539e9160b57cd75f0dd0c13fb41df71
nyal <nyalsadiq@hotmail.com>
2018-05-06 17:28:44 +0100

    -API Docs cleaned up

--- profiles/views.py
+++ profiles/views.py
@@ -6,11 +6,40 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class ProfileList(generics.ListCreateAPIView):
+    """
+    get:
+    Returns a list of profiles at low detail.
+
+    post:
+    Creates a new User and Profile.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    
     
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Get details of user with id = {id}. 
+    NOTE  user id is different to profile id.
+    Always use the user id when fetching data. Profile data is bundled with this
+    requests response.
+
+    put:
+    Update user details. To update profile details, send them in another dictionary,
+    for example {"username": "newname", "profile": {"bio": "cameraguy"}}
+
+    patch:
+    Update user details. To update profile details, send them in another dictionary,
+    for example {"username": "newname", "profile": {"bio": "cameraguy"}}
+
+    delete:
+    Delete user with id = {id}
+    """
+
     queryset = User.objects.all()
     serializer_class = UserDetailSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
--- projects/views.py
+++ projects/views.py
@@ -14,17 +14,27 @@ logger = logging.getLogger('views')
 
 @api_view(['GET'])
 def api_root(request, format=None):
+    """
+    Returns links to the projects and profiles directory.
+    """
     return Response({
         'profiles': reverse('profiles:index', request=request, format=format),
         'projects': reverse('projects:index', request=request, format=format)
     })
 
 class IndexView(generics.ListCreateAPIView):
+    """
+    post:
+    Creates a new project instance.
+    """
     queryset = Project.objects.all()
     serializer_class = ProjectDetailSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
     
     def get(self, request, format=None):
+        """
+        Returns a list of projects at low detail.
+        """
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
@@ -33,6 +43,21 @@ class IndexView(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 class ProjectView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Returns details of project with id = {id}
+
+    put:
+    Updates various fields of the project with id = {id}
+    Cannot be used to update requirements, use the requirements view instead.
+
+    patch:
+    Updates various fields of the project with id = {id}
+    Cannot be used to update requirements, use the requirements view instead.
+
+    delete:
+    Deletes project with id = {id}
+    """
     queryset = Project.objects.all()
     serializer_class = ProjectDetailSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
@@ -41,11 +66,28 @@ class ProjectView(generics.RetrieveUpdateDestroyAPIView):
         serializer.save(owner=self.request.user)
 
 class RequirementView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Returns requirement with id = {id}
+
+    put:
+    Updates the requirement with id = {id}
+
+    patch:
+    Updates the requirement with id = {id}
+
+    delete:
+    Deletes the requirement with id = {id}
+    """
     queryset = Requirement.objects.all()
     serializer_class = RequirementSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsRequestOwnerOrReadOnly)
 
     def post(self, request, *args, **kwargs):
+        """
+        Creates a new requirement for project with id = {id}
+        Must be owner of project to make a requirement.
+        """
         project_id = kwargs.get('pk')
         project = get_object_or_404(Project, pk=project_id)
     
@@ -57,11 +99,27 @@ class RequirementView(generics.RetrieveUpdateDestroyAPIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class ReactView(generics.RetrieveUpdateDestroyAPIView):
+    """
+        put:
+        Updates the comment with id = {id}
+        Send new text in "body" tag of request body.
+        
+        patch: 
+        Updates the comment with id = {id}
+        Send new text in "body" tag of request body.
+
+        delete:
+        Deletes the comment with id = {id}
+    """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
 
     def get(self, request, *args, **kwargs):
+        """
+        Leave a like for project with id = {id}.
+        Incremements "like" field by 1.
+        """
         project_id = kwargs.get('pk')
         project = get_object_or_404(Project, pk=project_id)    
 
@@ -74,6 +132,10 @@ class ReactView(generics.RetrieveUpdateDestroyAPIView):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, *args, **kwargs):
+        """
+        Leave a comment for project with id = {id}.
+        """
+    
         project_id = kwargs.get('pk')
         project = get_object_or_404(Project, pk=project_id)
         owner = request.user
