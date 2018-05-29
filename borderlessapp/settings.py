b794a5677a9654b37f430af5b5aa6267e4371966
nyal <nyalsadiq@hotmail.com>
2018-05-29 03:15:03 +0100

    f

--- borderlessapp/settings.py
+++ borderlessapp/settings.py
@@ -175,5 +175,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 #import django_heroku
 #django_heroku.settings(locals())
